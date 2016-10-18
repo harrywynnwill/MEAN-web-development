b9da09250f890c3c456e45405238d354843de8a8
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-10-18 17:43:53 +0100

    mongoose virtuals

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,10 +5,41 @@ var mongoose = require('mongoose'),
     firstName: String,
     lastName: String,
     email: String,
-    username: String,
-    password: String
+    username: {
+      type: String,
+      trim: true
+    },
+    password: String,
+    created: {
+      type: Date,
+      default: Date.now
+    },
+    website: {
+        type: String,
+        get: function(url) {
+          if (!url) {
+            return url;
+          } else {
+            if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+              url = 'http://' + url;
+            }
+        return url;
+      }
+    }
+  }
+
   });
 
+UserSchema.virtual('fullName').get(function(){
+  return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+  var splitName = fullName.split(' ');
+  this.firstName = splitName[0]  || '';
+  this.lastName = splitName[1] || '';
+});
+
+ UserSchema.set('toJSON', { getters: true, virtuals: true});
 mongoose.model('User', UserSchema);
 
 // curl -X POST -H "Content-Type: application/json" -d '{"firstName":"First", "lastName":"Last","email":"user@example.com","username":"username","password":"password"}' localhost:3000/users
+// curl -X PUT -H "Content-Type: application/json" -d '{"lastName":"Updated"}' localhost:3000/users/5801fd3ab5e5bf0b23dd5741
